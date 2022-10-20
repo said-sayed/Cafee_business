4aabc6d60c8f47d4e595bc8066ffb4c5efbaaba5
Said Sayed <90984453+said-sayed@users.noreply.github.com>
2022-10-20 18:34:36 +0200

    edit admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,7 +17,7 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        Admin::create([
             'name' => 'said sayed',
             'email' => 'said.sayed@gmail.com',
             'password' => Hash::make('12345678')
